dfe3fc2856a402bc0879d3ba5f31ec718dd722b3
Alessandro Macagno <Alagaesia93@users.noreply.github.com>
2024-06-14 10:59:55 +0200

    Initial Commit

---
+++ piazza/piazzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  piazzaApp.swift
+//  piazza
+//
+//  Created by Alessandro Macagno on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct piazzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
